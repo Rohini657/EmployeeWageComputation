f02ee3dd55a75cdc62a17b85f0091dca1e378b5a
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-24 14:43:02 +0530

    DailyWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,14 +6,21 @@ public class EmployeeWageComputation
 	{
 		System.out.println("*********Welcome EmployeeWage Computtaion************");
 		final int fullTime=1;
+		final int wagePerHour=20;
+		final int fullDayHour=8;
+		//variable
+		int dailyWage;
+		int dailyHours;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck == fullTime)
 		{
 			System.out.println("Employee is Present");
+			dailyHours=fullDayHour;
 		}
 		else
 		{
 			System.out.println("Employee is Absent");
+			dailyHours=0;
 		}
 	}
 }
