8253314352a5043cb0cfc9701cd47a0cba2f147f
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-24 14:28:07 +0530

    First UseCase

---
+++ EmployeeWage.java
@@ -0,0 +1,19 @@
+package com.Employee;
+
+public class EmployeeWageComputation 
+{
+	public static void main(String args[])
+	{
+		System.out.println("*********Welcome EmployeeWage Computtaion************");
+		final int fullTime=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck == fullTime)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
+	}
+}
