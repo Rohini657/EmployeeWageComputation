29d6603ab2c5ef88abff178eaaba4c584a6f5734
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-24 15:08:26 +0530

    MonthlyWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -12,19 +12,25 @@ public class EmployeeWageComputation
 		//variable
 		int dailyWage;
 		int dailyHours;
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		switch(empCheck){
-			case fullTime:
-				dailyHours=fullDayHour;
-			break;
-			case partTime:
-				dailyHours=fullDayHour/2;
-			break;
-			default:
-				dailyHours=0;
-			break;
+		int monthlyWage=0;
+		//computation
+		for( int day=0;day<-dayPerMonth;d++)
+		{
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			switch(empCheck){
+				case fullTime:
+					dailyHours=fullDayHour;
+					break;
+				case partTime:
+					dailyHours=fullDayHour/2;
+					break;
+				default:
+					dailyHours=0;
+				break;
 		}
 		dailyWage=wagePerHour*dailyHour;
 		System.out.println("Employee DailyWage= "+dailyWage);
+		monthlyWage=dailywage*20;
+		System.out.println("Employee Monthly wage="+monthlyWage)
 	}
 }
